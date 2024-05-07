71a26ac21553faea84bcda707205cbcd2a6e4b33
Mihai Eduard Petcu <103376036+mihaiep@users.noreply.github.com>
2024-05-07 23:26:31 +0300

    A fix on how CR is handled inside FileFormatter and few adjustments

--- melogger/format/_console.py
+++ melogger/format/_console.py
@@ -1,12 +1,33 @@
+import logging as _logging
 import re
+from copy import copy as _copy
 
-from ._file import _FileFormatter
 from ..colors import Colors as _Colors
-from ..utils import LevelData as _LevelData
+from ..utils import Levels, LevelData as _LevelData
 
 
-class _ConsoleFormatter(_FileFormatter):
+class _ConsoleFormatter(_logging.Formatter):
+    LINE_SEP = "\n"
+    ANSI_REGEX = r"\033\[[\d;]*[A-Za-z]"
+    _logging.addLevelName(Levels.PLAIN.value, "PLAIN")
+
+    def __init__(self, formats: dict, fmt=None, date_frmt=None, style="%", validate=True, *, defaults=None):
+        self.FORMATS = formats
+        super().__init__(fmt, date_frmt, style, validate, defaults=defaults)
+
+    def format(self, message) -> str:
+        cpy_msg = _copy(message)
+        level_data = self.FORMATS.get(cpy_msg.levelno)
+        cpy_msg.levelname = level_data.label
+        if hasattr(cpy_msg, 'crt_module'):
+            cpy_msg.module = cpy_msg.crt_module
+        if hasattr(cpy_msg, 'crt_method_name'):
+            cpy_msg.funcName = cpy_msg.crt_method_name
+        cpy_msg.terminator = cpy_msg.end if hasattr(cpy_msg, 'end') else self.LINE_SEP
+        self.custom_format(cpy_msg, level_data)
+        return _logging.Formatter(level_data.text_format).format(cpy_msg)
+
     def custom_format(self, cpy_message, level_data: _LevelData) -> None:
         cpy_message.start = "" if not hasattr(cpy_message, "start") else cpy_message.start
         cpy_message.col_end = _Colors.END
-        cpy_message.col_start = level_data.color if not hasattr(cpy_message, 'col_start') else "".join(re.findall(self.ANIS_REGEX, cpy_message.col_start))
+        cpy_message.col_start = level_data.color if not hasattr(cpy_message, 'col_start') else "".join(re.findall(self.ANSI_REGEX, cpy_message.col_start))
--- tests/test_builder.py
+++ tests/test_builder.py
@@ -10,22 +10,22 @@ class LoggerTest(unittest.TestCase):
     LOG1_FORMAT = r'^<{}> \(\d+\) {}$'
     LOG2_FORMAT = r'^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d \[{}] \w+ \(\d+\) {}$'
 
+    FORMATS = {
+        Levels.DEBUG.value: LevelData("DEBUG", Colors.COL.GREY, "<%(levelname)s> (%(process)d) %(message)s%(terminator)s"),
+        Levels.INFO.value: LevelData("INFO", Colors.COL.DEFAULT, "<%(levelname)s> (%(process)d) %(message)s%(terminator)s"),
+        Levels.WARN.value: LevelData("WARN", Colors.COL.YELLOW, "<%(levelname)s> (%(process)d) %(message)s%(terminator)s"),
+        Levels.ERROR.value: LevelData("ERROR", Colors.COL.RED, "<%(levelname)s> (%(process)d) %(message)s%(terminator)s"),
+        Levels.CRITICAL.value: LevelData("CRITICAL", Colors.COL.PURE.RED, "<%(levelname)s> (%(process)d) %(message)s%(terminator)s"),
+        Levels.PLAIN.value: LevelData("PLAIN", Colors.COL.DEFAULT, "%(start)s%(col_start)s%(message)s%(terminator)s")
+    }
+
     @classmethod
     def tearDownClass(cls):
         os.remove('test1.log')
         os.remove('test2.log')
 
     def test_build_logger(self):
-        FORMATS = {
-            Levels.DEBUG.value: LevelData("DEBUG", Colors.COL.GREY, "<%(levelname)s> (%(process)d) %(message)s%(terminator)s"),
-            Levels.INFO.value: LevelData("INFO", Colors.COL.DEFAULT, "<%(levelname)s> (%(process)d) %(message)s%(terminator)s"),
-            Levels.WARN.value: LevelData("WARN", Colors.COL.YELLOW, "<%(levelname)s> (%(process)d) %(message)s%(terminator)s"),
-            Levels.ERROR.value: LevelData("ERROR", Colors.COL.RED, "<%(levelname)s> (%(process)d) %(message)s%(terminator)s"),
-            Levels.CRITICAL.value: LevelData("CRITICAL", Colors.COL.PURE.RED, "<%(levelname)s> (%(process)d) %(message)s%(terminator)s"),
-            Levels.PLAIN.value: LevelData("PLAIN", Colors.COL.DEFAULT, "%(start)s%(col_start)s%(message)s%(terminator)s")
-        }
-
-        logger1 = LoggerBuilder.get_logger("test1", level=Levels.DEBUG, formats=FORMATS, file_name="test1.log", file_mode='w')
+        logger1 = LoggerBuilder.get_logger("test1", level=Levels.DEBUG, formats=LoggerTest.FORMATS, file_name="test1.log", file_mode='w')
         logger2 = LoggerBuilder.get_logger("test2", level=Levels.DEBUG, file_name="test2.log", file_mode='w')
         self.assertEqual(logger1.name, 'test1')
         self.assertEqual(logger2.name, 'test2')
@@ -34,10 +34,11 @@ class LoggerTest(unittest.TestCase):
         logger2.warning("message info test2")
 
         del logger1, logger2
-        with open('test1.log', 'r') as f: lines1 = f.read().splitlines()
-        with open('test2.log', 'r') as f: lines2 = f.read().splitlines()
-
-        self.assertRegex(lines1[0], LoggerTest.LOG1_FORMAT.format('INFO', 'Execution started'))
-        self.assertRegex(lines1[1], LoggerTest.LOG1_FORMAT.format('WARN', 'message info test1'))
-        self.assertRegex(lines2[0], LoggerTest.LOG2_FORMAT.format('INFO', 'Execution started'))
-        self.assertRegex(lines2[1], LoggerTest.LOG2_FORMAT.format('WARN', 'message info test2'))
+        with open('test1.log', 'r') as f:
+            lines1 = f.read().splitlines()
+            self.assertRegex(lines1[0], LoggerTest.LOG1_FORMAT.format('INFO', 'Execution started'))
+            self.assertRegex(lines1[1], LoggerTest.LOG1_FORMAT.format('WARN', 'message info test1'))
+        with open('test2.log', 'r') as f:
+            lines2 = f.read().splitlines()
+            self.assertRegex(lines2[0], LoggerTest.LOG2_FORMAT.format('INFO', 'Execution started'))
+            self.assertRegex(lines2[1], LoggerTest.LOG2_FORMAT.format('WARN', 'message info test2'))
